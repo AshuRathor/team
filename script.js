1f0a773eed6e698e99215047ad19c8ad6ebfbfc2
AshuRathor <97465069+AshuRathor@users.noreply.github.com>
2022-10-12 09:41:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector(".abt_us").addEventListener("mouseover", function () {
+    document.querySelector(".abt_us_within").classList.add("show")
+})
+
+document.querySelector(".abt_us").addEventListener("mouseout", function () {
+    document.querySelector(".abt_us_within").classList.remove("show")
+})
+
+document.querySelector(".cars").addEventListener("mouseover", function () {
+    document.querySelector(".cars_within").classList.add("show")
+})
+
+document.querySelector(".cars").addEventListener("mouseout", function () {
+    document.querySelector(".cars_within").classList.remove("show")
+})
+
