69020ec21974aae4d1bdbdd0f18965a8af5db8c5
AshuRathor <97465069+AshuRathor@users.noreply.github.com>
2022-10-13 18:31:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ document.querySelector(".cars").addEventListener("mouseout", function () {
     document.querySelector(".cars_within").classList.remove("show")
 })
 
+document.querySelector(".navbar-brand").addEventListener("mouseover", function () {
+    document.querySelector("#navbar-brandImg").setAttribute("src", "img/logo_non_inverted.png")
+    document.querySelector(".navbar-brand").style.background = "white"
+    document.querySelector("#navbar-brandHeading").style.color = "#232323"
+
+
+
+})
+
+document.querySelector(".navbar-brand").addEventListener("mouseout", function () {
+    document.querySelector("#navbar-brandImg").setAttribute("src", "img/logo.png")
+    document.querySelector(".navbar-brand").style.background = "none"
+    document.querySelector("#navbar-brandHeading").style.color = "white"
+
+
+})
+
